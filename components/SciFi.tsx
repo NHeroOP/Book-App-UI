d3f1181de2bc5195c9af2f4a8ed073af53dfbe4a
NHeroOP <neeravtheheroo@gmail.com>
2024-01-17 00:16:26 +0530

    Create components and add starter code in it

---
+++ components/SciFi.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function SciFi() {
+  return (
+    <View>
+      <Text>SciFi</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ components/footer.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function footer() {
+  return (
+    <View>
+      <Text>footer</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
